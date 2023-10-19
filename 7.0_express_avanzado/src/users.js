2122844c318a5bc2361ecdfe9c07ed028ad3b31d
Daniel Henao <danior-0208@hotmail.com>
2023-10-19 16:17:36 -0500

    Express Avanzado

---
+++ 7.0_express_avanzado/src/users.js
@@ -0,0 +1,30 @@
+export const users = [
+  {
+    firstName: "Samuel",
+    lastName: "Henao",
+    email: "presidente@example.com",
+    password: "samu0208",
+    id: 1,
+  },
+  {
+    firstName: "Diana",
+    lastName: "Quevedo",
+    email: "presidente@example.com",
+    password: "samu0208",
+    id: 3,
+  },
+  {
+    firstName: "Daniela",
+    lastName: "Henao",
+    email: "financiera@example.com",
+    password: "Daniel0208",
+    id: 4,
+  },
+  {
+    firstName: "Daniel",
+    lastName: "Henao",
+    email: "gerencia@example.com",
+    password: "Dani12345",
+    id: 5,
+  },
+];
